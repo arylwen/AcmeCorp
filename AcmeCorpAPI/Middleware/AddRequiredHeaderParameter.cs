50c7e74ac3b028ad8dffc86870d5c5bccb17dd64
arylwen <gabriela@halcyon-solutions.com>
2024-04-28 09:07:20 -0700

    api key access

---
+++ AcmeCorpAPI/Middleware/AddRequiredHeaderParameter.cs
@@ -0,0 +1,23 @@
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+ public class AddRequiredHeaderParameter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+
+            if (operation.Parameters == null)
+                operation.Parameters = new List<OpenApiParameter>();
+
+            operation.Parameters.Add(new OpenApiParameter()
+            {
+                Name = "X-Api-Key",
+                Description = "API Key",
+                In = ParameterLocation.Header,
+                Schema = new OpenApiSchema() { Type = "String" },
+                //Required = true,
+                Example = new OpenApiString("pgH7QzFHJx4w46fI~5Uzi4RvtTwlEXp")
+            });
+        }
+    }
--- AcmeCorpAPI/Program.cs
+++ AcmeCorpAPI/Program.cs
@@ -7,19 +7,28 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AcmeCorpAPIContext>();
     //(opt =>opt.UseInMemoryDatabase("AcmeCorpAPI"));
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.OperationFilter<AddRequiredHeaderParameter>();
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options => // UseSwaggerUI is called only in Development.
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseMiddleware<ApiKeyMiddleware>();
 
 app.MapControllers();
 
